ee19a49f69aef6dee6e62aeae5b2af613da1d139
Ismael Castro <Ismaelgabrijob@hotmail.com>
2022-11-20 20:22:35 -0300

    First Push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ContentComponent } from './content/content.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ContentComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
